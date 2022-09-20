925e6d1f5d8014e05425d81accb03b51860d98d7
chabbe76 <chabbe76@gmail.com>
2022-09-20 21:49:58 +0200

    Första Input

---
+++ app/src/main/java/com/example/cardapplication/Cards.kt
@@ -0,0 +1,25 @@
+package com.example.cardapplication
+
+class Cards {
+
+    val name : String=""
+    val points: Int=0
+    val image : Int=0
+
+//Kan jag lägga till en varibel med bilder.
+
+
+    val cardlist = mutableListOf<Cards>()
+    val c1 = Cards()
+
+
+
+}
+
+
+
+
+
+
+//val cards = Cards()
+//Ska jag ha eget klass? Måste ge värde till varje kort...
